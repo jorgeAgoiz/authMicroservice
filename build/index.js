0eca5996c512f5bab27cb318fe910d900a6c7d70
jorgeAP <jorgeagoiz@gmail.com>
2021-07-09 17:15:52 +0200

    builded

--- build/index.js
+++ build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("lo que sea");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var env_vars_1 = require("./util/env.vars");
+var mysql_config_1 = __importDefault(require("./util/mysql.config"));
+var app = express_1.default();
+mysql_config_1.default
+    //   .sync({ force: true })
+    .sync()
+    .then(function (result) {
+    app.listen(env_vars_1.PORT, function () {
+        console.log("Listening in port " + env_vars_1.PORT + "....");
+    });
+})
+    .catch(function (err) { return console.log(err); });
---
+++ build/util/env.vars.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DBPORT = exports.DBPASS = exports.DBUSER = exports.DBNAME = exports.PORT = void 0;
+var dotenv_1 = __importDefault(require("dotenv"));
+if (process.env.NODE_ENV !== "production") {
+    dotenv_1.default.config();
+}
+exports.PORT = process.env.PORT;
+exports.DBNAME = process.env.DB_NAME;
+exports.DBUSER = process.env.DB_USER;
+exports.DBPASS = process.env.DB_PASS;
+exports.DBPORT = process.env.DB_PORT;
